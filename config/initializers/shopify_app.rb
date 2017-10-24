050a27a88b91b50342e943f8df20fff03614c40a
Luis Morales <davidmorales257@gmail.com>
2017-10-23 22:16:42 -0400

    add cart webhook

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -6,4 +6,7 @@ ShopifyApp.configure do |config|
   config.embedded_app = true
   config.after_authenticate_job = false
   config.session_repository = Shop
+  config.webhooks = [
+    {topic: 'carts/update', address:'https://e32239be.ngrok.io/webhooks/carts_update', format: 'json'}
+  ]
 end
