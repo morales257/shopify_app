97d205bd913d460f81320cf741ec27407c7ce65b
Luis Morales <davidmorales257@gmail.com>
2017-10-23 22:11:32 -0400

    generate shopify app

---
+++ config/initializers/shopify_app.rb
@@ -0,0 +1,9 @@
+ShopifyApp.configure do |config|
+  config.application_name = "My Shopify App"
+  config.api_key = "9d3b5be0d7587a3d2b934a0200cbc5ff"
+  config.secret = "70848228fe5a37a81be775c55b310c77"
+  config.scope = "read_orders,read_checkouts"
+  config.embedded_app = true
+  config.after_authenticate_job = false
+  config.session_repository = Shop
+end
