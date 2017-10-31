4b000f8af0088632e34cf2953b4c3e9ddd8aa76a
Luis Morales <davidmorales257@gmail.com>
2017-10-31 19:53:14 -0400

    add pry to debug

--- Gemfile
+++ Gemfile
@@ -15,6 +15,8 @@ gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.2'
 #get http request
 gem 'httparty'
+#use pry
+gem 'pry'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
 
--- app/jobs/checkouts_update_job.rb
+++ app/jobs/checkouts_update_job.rb
@@ -1,4 +1,7 @@
+require 'pry'
+
 class CheckoutsUpdateJob < ActiveJob::Base
+
   def perform(shop_domain:, webhook:)
     shop = Shop.find_by(shopify_domain: shop_domain)
     shop.with_shopify_session do
@@ -13,6 +16,7 @@ class CheckoutsUpdateJob < ActiveJob::Base
                       email: webhook[:email],
                       discount_codes: webhook[:discount_codes][:code]
                     }
+         binding.pry
         puts "creating new checkout"
         new_checkout = Checkout.where(checkout_id: checkout[:checkout_id]).first_or_create(checkout)
         shop.checkouts << new_checkout
