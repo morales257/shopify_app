7e3717c44267ac10472a276bcfc6ae7c97f185c7
Luis Morales <davidmorales257@gmail.com>
2017-10-31 17:27:33 -0400

    change webhook controller

--- app/jobs/checkouts_update_job.rb
+++ app/jobs/checkouts_update_job.rb
@@ -4,19 +4,21 @@ class CheckoutsUpdateJob < ActiveJob::Base
     shop.with_shopify_session do
       puts "starting session"
       if webhook[:shipping_address][:phone]
-        puts "creating AC"
-        checkout = {
-                      checkout_id: webhook[:id],
-                      phone: webhook[:shipping_address][:phone],
-                      first_name: webhook[:shipping_address][:first_name],
-                      last_name: webhook[:shipping_address][:last_name],
-                      email: webhook[:email],
-                      discount_codes: webhook[:discount_codes][:code]
-                    }
+        #puts "creating AC"
+        #checkout = {
+        #              checkout_id: webhook[:id],
+        #              phone: webhook[:shipping_address][:phone],
+        #              first_name: webhook[:shipping_address][:first_name],
+        #              last_name: webhook[:shipping_address][:last_name],
+        #              email: webhook[:email],
+        #              discount_codes: webhook[:discount_codes][:code]
+        #            }
         puts "creating new checkout"
-        new_checkout = Checkout.where(checkout_id: checkout[:checkout_id]).first_or_create(checkout)
+        new_checkout = Checkout.where(checkout_id: webook[:id]).first_or_create(
+        phone: webhook[:shipping_address][:phone], first_name: webhook[:shipping_address][:first_name],
+        last_name: webhook[:shipping_address][:last_name], email: webhook[:email], discount_codes: webhook[:discount_codes][:code])
         shop.checkouts << new_checkout
-        puts "Saved new checkout" if new_checkout.saved?
+        puts "Saved new checkout"
       else
         puts "This checkout does not have a phone number"
       end
