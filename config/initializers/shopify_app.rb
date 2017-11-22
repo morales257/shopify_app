fcc1cc9e23be66601ae4866a1d6445d81afa1e90
Luis Morales <davidmorales257@gmail.com>
2017-11-21 23:24:24 -0500

    fix env variable

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,7 +1,7 @@
 ShopifyApp.configure do |config|
   config.application_name = "My Shopify App"
   config.api_key = ENV['API_KEY']
-  config.secret = EBV['SECRET']
+  config.secret = ENV['SECRET']
   config.scope = "read_orders,read_checkouts, write_products"
   config.embedded_app = true
   config.after_authenticate_job = false
